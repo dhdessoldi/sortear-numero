cf307bf704d9f010e14fb76c306663bb7808ac3b
dhdessoldi <dhdessoldi@hotmail.com>
2022-12-07 16:14:01 -0300

    projeto concluído

---
+++ js/validacao.js
@@ -0,0 +1,28 @@
+function validaChute(chute) {
+    const numero = +chute;
+
+    if(chuteInvalido(numero)) {
+        elementoChute.innerHTML += `<div>Valor inválido! Fale um número por favor!</div>`
+        
+    }
+    if (numeroMaiorOuMenorPermitido(numero)){
+        elementoChute.innerHTML += `<div>Valor inválido! O número secreto está entre ${menorValor} e ${maiorValor}</div>`
+        
+    }
+
+    if(numero === numeroSecreto) {
+        document.body.innerHTML = `<h2>Você acertou!</h2><h3>O número secreto era ${numeroSecreto}</h3>`
+    } else if (numero > numeroSecreto){
+        elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-sharp fa-solid fa-arrow-down"></i></div>`
+    } else {
+        elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-sharp fa-solid fa-arrow-up"></i></div>`
+    };
+}
+
+function chuteInvalido(numero) {
+    return Number.isNaN(numero);
+}
+
+function numeroMaiorOuMenorPermitido(numero) {
+    return numero > maiorValor || numero < menorValor;
+}
